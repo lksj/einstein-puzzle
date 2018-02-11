ef34918aa67a3c31061384024f81813906f39ded
Jordan Evens <jordan.evens@gmail.com>
2018-02-11 15:33:50 -0500

    fixed lexal.cpp code by matching to code from mkres

--- lexal.cpp
+++ lexal.cpp
@@ -3,6 +3,8 @@
 // Einstein Puzzle
 // Copyright (C) 2003-2005  Flowix Games
 
+// Modified 2018-02-11 by Jordan Evens <jordan.evens@gmail.com>
+
 // Einstein Puzzle is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
@@ -113,6 +115,7 @@ Lexeme Lexal::readString(int startLine, int startPos, wchar_t quote)
         if ('\n' == ch) {
             line++;
             pos = 0;
+            str += ch;
         } else if ('\\' == ch) {
             if (! reader.isEof()) {
                 wchar_t nextCh = reader.getNextChar();
